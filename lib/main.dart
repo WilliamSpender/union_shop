9c9eb60922e02e2bf21f3a32686b4be7c24785b2
William <up2195767@myport.ac.uk>
2025-12-02 16:01:38 +0000

    Added routing to about us page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:union_shop/about_us_page.dart';
 import 'package:union_shop/ui_constructors.dart';
 import 'package:union_shop/product_page.dart';
 
@@ -22,7 +23,8 @@ class UnionShopApp extends StatelessWidget {
       initialRoute: '/',
       // When navigating to '/product', build and return the ProductPage
       // In your browser, try this link: http://localhost:49856/#/product
-      routes: {'/product': (context) => const ProductPage()},
+      routes: {'/product': (context) => const ProductPage(),
+        '/about-us': (context) => const AboutUsPage()},
     );
   }
 }
