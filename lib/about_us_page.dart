d18fda3b54ae95458b69203e172b3e4f88e77137
William <up2195767@myport.ac.uk>
2025-12-02 16:34:29 +0000

    Theft of the uni about us page

--- lib/about_us_page.dart
+++ lib/about_us_page.dart
@@ -57,7 +57,7 @@ class AboutUsPage extends StatelessWidget {
                       ),
                     ),
                     SizedBox(height: 48),
-                    Text(about_us_message,
+                    Text(aboutUsMessage,
                       style: TextStyle(
                         fontSize: 16,
                         color: Colors.grey,
@@ -77,7 +77,17 @@ class AboutUsPage extends StatelessWidget {
     );
   }
 
-  static const String about_us_message = '''
-  
+  static const String aboutUsMessage = '''
+  Welcome to the Union Shop!
+
+We’re dedicated to giving you the very best University branded products, with a range of clothing and merchandise available to shop all year round! We even offer an exclusive personalisation service!
+
+All online purchases are available for delivery or instore collection!
+
+We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don’t hesitate to contact us at hello@upsu.net.
+
+Happy shopping!
+
+The Union Shop & Reception Team
 ''';
 }
