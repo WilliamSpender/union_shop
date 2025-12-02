f09bcce944e06887b8d39a8401a3a25dbbad6a22
William <up2195767@myport.ac.uk>
2025-12-02 15:55:08 +0000

    Created about us page

---
+++ lib/about_us_page.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+import 'ui_constructors.dart';
+
+class ProductPage extends StatelessWidget {
+  const ProductPage({super.key});
+
+  void navigateToHome(BuildContext context) {
+    Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
+  }
+
+  void placeholderCallbackForButtons() {
+    // This is the event handler for buttons that don't work yet
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Header
+            Container(
+              height: 100,
+              color: Colors.white,
+              child: Column(
+                children: [
+                  // Top banner
+                  Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    color: const Color(0xFF4d2963),
+                    child: const Text(
+                      'PLACEHOLDER HEADER TEXT',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ),
+                  // Main header
+                  generateMainHeader(context, navigateToHome, placeholderCallbackForButtons,
+                          (ctx) {}, (ctx) {}),
+                ],
+              ),
+            ),
+
+            //Main body
+            Container(
+              height: 100,
+              color: Colors.white,
+              child: const Column(
+                children: [
+                  Text(
+                    'About Us',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                  Text(
+                    'about us details placeholder',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+
+            // Footer
+            generateFooter(context),
+          ],
+        ),
+      ),
+    );
+  }
+}
