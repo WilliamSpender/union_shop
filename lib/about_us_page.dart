c31680d056c0fb05d5e245ab4aee852b010634d1
William <up2195767@myport.ac.uk>
2025-12-02 16:09:32 +0000

    Removed unnececary navigate home function

--- lib/about_us_page.dart
+++ lib/about_us_page.dart
@@ -5,10 +5,6 @@ import 'ui_constructors.dart';
 class AboutUsPage extends StatelessWidget {
   const AboutUsPage({super.key});
 
-  void navigateToHome(BuildContext context) {
-    Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
-  }
-
   void placeholderCallbackForButtons() {
     // This is the event handler for buttons that don't work yet
   }
