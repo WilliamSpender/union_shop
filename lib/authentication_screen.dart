b4c8d5ae369de5ed48750610247743c522d54ac7
William <up2195767@myport.ac.uk>
2025-12-04 17:40:35 +0000

    Created authentication screen

---
+++ lib/authentication_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'ui_constructors.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return;
+  }
+
+}
