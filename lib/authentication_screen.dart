697d85c86f3bde08f15d6739321b0403552cd531
William <up2195767@myport.ac.uk>
2025-12-04 17:59:25 +0000

    Set up decoration and padding

--- lib/authentication_screen.dart
+++ lib/authentication_screen.dart
@@ -7,7 +7,26 @@ class AuthPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return;
+    return Scaffold(
+      backgroundColor: Colors.grey[150],
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(
+              color: Colors.grey.shade300,
+              width: 0.5,
+            ),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child:
+            Text(
+              'Authentication Screen Placeholder',
+              style: TextStyle(fontSize: 24, color: Colors.grey[600]),
+            ),
+        ),
+      ),
+    );
   }
-
 }
