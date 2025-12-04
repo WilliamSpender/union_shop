893c7c47012cb88a5e1771acc8df217242194242
William <up2195767@myport.ac.uk>
2025-12-04 21:13:46 +0000

    Created dummy cartPage class

---
+++ lib/views/cart_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'package:union_shop/views/ui_constructors.dart';
+
+import '../models/cart/cart_model.dart';
+
+class CartPage extends StatelessWidget {
+  final Cart cart;
+
+  const CartPage({super.key, required this.cart});
+
+  @override
+  Widget build(BuildContext context) {
+    return GenericPage(bodyContent: [
+
+    ]);
+  }
+}
