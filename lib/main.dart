ddff3eac76c2426e42b4e86ca4b43731bb5b121b
William <up2195767@myport.ac.uk>
2025-12-05 06:44:57 +0000

    Navigate to collections when browse products button is pressed

--- lib/main.dart
+++ lib/main.dart
@@ -117,7 +117,7 @@ class HomeScreen extends StatelessWidget {
                         ),
                         const SizedBox(height: 32),
                         ElevatedButton(
-                          onPressed: placeholderCallbackForButtons,
+                          onPressed: () => navigateToGeneric(context, '/collections'),
                           style: ElevatedButton.styleFrom(
                             backgroundColor: const Color(0xFF4d2963),
                             foregroundColor: Colors.white,
