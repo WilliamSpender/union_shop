6a5ce5f9ac250572907150676596c047183f7e97
William <up2195767@myport.ac.uk>
2025-12-05 01:00:04 +0000

    Made use of default extraCost value

--- lib/presets/customisation_presets.dart
+++ lib/presets/customisation_presets.dart
@@ -1,15 +1,15 @@
 import '../models/item_customisation_models.dart';
 
 final ItemCustomisation shirtSizes = ItemCustomisation(defaultValueIndex: 1, name: 'Size', description: 'Shirt Size', options: [
-  Customisation(value: 'Small', extraCost: 0.0),
-  Customisation(value: 'Medium', extraCost: 0.0),
+  Customisation(value: 'Small'),
+  Customisation(value: 'Medium'),
   Customisation(value: 'Large', extraCost: 1.0),
   Customisation(value: 'X-Large', extraCost: 2.0),
 ]);
 
 final ItemCustomisation colours = ItemCustomisation(defaultValueIndex: 1, name: 'Size', description: 'Shirt Size', options: [
-  Customisation(value: 'Small', extraCost: 0.0),
-  Customisation(value: 'Medium', extraCost: 0.0),
-  Customisation(value: 'Large', extraCost: 0.0),
-  Customisation(value: 'X-Large', extraCost: 0.0),
+  Customisation(value: 'Small'),
+  Customisation(value: 'Medium'),
+  Customisation(value: 'Large'),
+  Customisation(value: 'X-Large'),
 ]);
