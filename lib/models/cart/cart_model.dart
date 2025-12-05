745020b7eb65c4e427a1f2558f86bb7fc3b6d7db
William <up2195767@myport.ac.uk>
2025-12-05 01:31:26 +0000

    Refactor Cart class to CartModel and update total price calculation

--- lib/models/cart/cart_model.dart
+++ lib/models/cart/cart_model.dart
@@ -2,15 +2,16 @@
 
 import 'cart_item_model.dart';
 
-class Cart {
-  final List<CartItem> items = [];
+class CartModel {
+  final List<CartItemModel> items = [];
+  double price = 0.0;
 
-  double totalPrice() {
-    double total = 0.0;
+
+  void updateTotalPrice() {
+    price = 0.0;
     for (var cartItem in items) {
-      total += cartItem.price;
+      price += cartItem.price;
     }
-    return total;
   }
 
 
