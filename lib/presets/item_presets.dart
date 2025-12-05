ba3b948df320044cf54543feb95a8427f5dc84d5
William <up2195767@myport.ac.uk>
2025-12-05 00:51:23 +0000

    Added preset entries for a hoodie, Tshirt and magnet

--- lib/presets/item_presets.dart
+++ lib/presets/item_presets.dart
@@ -1,16 +1,43 @@
 
-import '../models/item_customisation_models.dart';
 
-final ItemCustomisation shirtSizes = EnumCustomisation(defaultValueIndex: 1, name: 'Size', description: 'Shirt Size', options: [
-Customisation(value: 'Small', extraCost: 0.0),
-Customisation(value: 'Medium', extraCost: 0.0),
-Customisation(value: 'Large', extraCost: 1.0),
-Customisation(value: 'X-Large', extraCost: 2.0),
-]);
+import '../models/item_model.dart';
+import 'customisation_presets.dart';
 
-final ItemCustomisation colours = EnumCustomisation(defaultValueIndex: 1, name: 'Size', description: 'Shirt Size', options: [
-  Customisation(value: 'Small', extraCost: 0.0),
-  Customisation(value: 'Medium', extraCost: 0.0),
-  Customisation(value: 'Large', extraCost: 0.0),
-  Customisation(value: 'X-Large', extraCost: 0.0),
-]);
+final Item uopHoodie = Item(
+  name: "UOP Hoodie",
+  price: 35.00,
+  description: "A comfy UOP branded hoodie",
+  imageLocation: "https://shop.upsu.net/cdn/shop/files/Ivory_Hoodie_1024x1024@2x.png?v=1745583522",
+  customisationOptions: [
+    shirtSizes,
+    colours
+    ],
+);
+
+
+final Item portsmouthTShirt = Item(
+  name: "Portsmouth TShirt",
+  price: 18.00,
+  description: "Essential T-Shirt",
+  imageLocation: "https://shop.upsu.net/cdn/shop/files/Sage_T-shirt_1024x1024@2x.png?v=1759827236",
+  customisationOptions: [
+    shirtSizes,
+    colours
+  ],
+);
+
+final Item portsmouthCityMagnet = Item(
+  name: "Portsmouth City Magnet",
+  price: 4.50,
+  description:
+  """
+  Bring a bit of Portsmouth pride to your fridge, locker, or pinboard with our eye-catching Portsmouth City Magnet, featuring the artwork of renowned illustrator Julia Gash.
+
+  Part of our Portsmouth City Collection, this magnet showcases Julia’s iconic hand-drawn design, celebrating the city’s most loved landmarks and seaside charm in her signature playful style.
+
+Made from durable tin plate, it's both lightweight and long-lasting - a perfect souvenir or small gift with a big personality.
+""",
+  imageLocation: "https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282",
+  customisationOptions: [
+  ],
+);
