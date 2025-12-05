5eea4fec0aed8375398e67c93b24873235a35942
William <up2195767@myport.ac.uk>
2025-12-05 00:59:30 +0000

    used newly Combined ItemCustomisation

--- lib/presets/customisation_presets.dart
+++ lib/presets/customisation_presets.dart
@@ -1,13 +1,13 @@
 import '../models/item_customisation_models.dart';
 
-final ItemCustomisation shirtSizes = EnumCustomisation(defaultValueIndex: 1, name: 'Size', description: 'Shirt Size', options: [
+final ItemCustomisation shirtSizes = ItemCustomisation(defaultValueIndex: 1, name: 'Size', description: 'Shirt Size', options: [
   Customisation(value: 'Small', extraCost: 0.0),
   Customisation(value: 'Medium', extraCost: 0.0),
   Customisation(value: 'Large', extraCost: 1.0),
   Customisation(value: 'X-Large', extraCost: 2.0),
 ]);
 
-final ItemCustomisation colours = EnumCustomisation(defaultValueIndex: 1, name: 'Size', description: 'Shirt Size', options: [
+final ItemCustomisation colours = ItemCustomisation(defaultValueIndex: 1, name: 'Size', description: 'Shirt Size', options: [
   Customisation(value: 'Small', extraCost: 0.0),
   Customisation(value: 'Medium', extraCost: 0.0),
   Customisation(value: 'Large', extraCost: 0.0),
